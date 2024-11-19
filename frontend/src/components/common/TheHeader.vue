f1771c05efa0b83457a9222d6cd536268ce24d44
puranium235 <puranium235@gmail.com>
2024-11-19 17:05:41 +0900

    feat: kakao ì로 로그인 클릭시¸가코드 요청

--- frontend/src/components/common/TheHeader.vue
+++ frontend/src/components/common/TheHeader.vue
@@ -1,11 +1,14 @@
 <template>
   <header class="header">
     <h1>너만모르조?</h1>
-    <nav>문제등록 | 로그인 | 내정보</nav>
+    <nav>문제등록 |
+         <span @click="getKakaoAuth">로그인</span>
+         | 내정보</nav>
   </header>
 </template>
 
 <script setup>
+import { getKakaoAuth } from '@/api/oauthApi.js'
 
 </script>
 
