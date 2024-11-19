90cfaf5a738edc78bf87241688e4f061bc05ecb2
puranium235 <puranium235@gmail.com>
2024-11-19 17:11:35 +0900

    feat: 로그인 정보 pinia에 저장해 관리

--- frontend/src/components/common/TheHeader.vue
+++ frontend/src/components/common/TheHeader.vue
@@ -2,13 +2,18 @@
   <header class="header">
     <h1>너만모르조?</h1>
     <nav>문제등록 |
-         <span @click="getKakaoAuth">로그인</span>
-         | 내정보</nav>
+      <span v-if="user.isLoggedIn" @click="getKakaoAuth">로그인</span>
+      <span v-else>로그아웃</span>
+         | 내정보
+    </nav>
   </header>
 </template>
 
 <script setup>
 import { getKakaoAuth } from '@/api/oauthApi.js'
+import { useUser } from '@/stores/user.js'
+
+const user = useUser()
 
 </script>
 
