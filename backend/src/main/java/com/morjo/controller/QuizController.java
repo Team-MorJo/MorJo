cabb374cb3ac44e16fd747f1b6d5a1206759f49c
bang <sybang98@gmail.com>
2024-11-20 09:50:52 +0900

    feat: 퀴즈 등록기능 api 작성

--- backend/src/main/java/com/morjo/controller/QuizController.java
+++ backend/src/main/java/com/morjo/controller/QuizController.java
@@ -1,54 +1,69 @@
 package com.morjo.controller;
 
-import com.morjo.model.dto.Quiz;
-import com.morjo.model.dto.QuizResult;
-import com.morjo.model.service.QuizService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.morjo.model.dto.Quiz;
+import com.morjo.model.dto.QuizResult;
+import com.morjo.model.service.QuizService;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
-    private final QuizService quizService;
+	private final QuizService quizService;
 
-    // !TODO 유저정보를 받아서 안 푼 문제만 주게 수정필요
-    @GetMapping("/random")
-    public ResponseEntity<?> getRandomQuiz() {
-        Quiz quiz = quizService.getQuizRandom();
+	// !TODO 유저정보를 받아서 안 푼 문제만 주게 수정필요
+	@GetMapping("/random")
+	public ResponseEntity<?> getRandomQuiz() {
+		Quiz quiz = quizService.getQuizRandom();
+		
+		if (quiz == null) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body("퀴즈가 없습니다");
+		}
 
-        if (quiz == null) {
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("퀴즈가 없습니다");
-        }
+		return ResponseEntity.status(HttpStatus.OK).body(quiz);
+	}
 
-        return ResponseEntity.status(HttpStatus.OK).body(quiz);
-    }
+	@GetMapping("/{quizId}")
+	public ResponseEntity<?> getSpecificQuiz(@PathVariable("quizId") int quizId) {
+		Quiz quiz = quizService.getQuizById(quizId);
 
-    @GetMapping("/{quizId}")
-    public ResponseEntity<?> getSpecificQuiz(@PathVariable("quizId") int quizId) {
-        Quiz quiz = quizService.getQuizById(quizId);
+		if (quiz == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 번호의 퀴즈를 찾을 수 없습니다");
+		}
 
-        if (quiz == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 번호의 퀴즈를 찾을 수 없습니다");
-        }
+		return ResponseEntity.status(HttpStatus.OK).body(quiz);
+	}
 
-        return ResponseEntity.status(HttpStatus.OK).body(quiz);
-    }
+	@GetMapping("/{quizId}/result")
+	public ResponseEntity<?> getQuizResult(@PathVariable("quizId") int quizId) {
+		QuizResult result = quizService.getQuizResultById(quizId);
 
-    @GetMapping("/{quizId}/result")
-    public ResponseEntity<?> getQuizResult(@PathVariable("quizId") int quizId) {
-        QuizResult result = quizService.getQuizResultById(quizId);
+		if (result == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 번호의 퀴즈를 찾을 수 없습니다");
+		}
 
-        if (result == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("해당 번호의 퀴즈를 찾을 수 없습니다");
-        }
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
 
-        return ResponseEntity.status(HttpStatus.OK).body(result);
-    }
+	@PostMapping("/create")
+	public ResponseEntity<?> createQuiz(@RequestBody Quiz quiz) {
+		long quizId = quizService.createQuiz(quiz);
+
+		if (quizId == -1) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("본문, 선택지 또는 정답 값에 오류가 있습니다.");
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(quizId);
+	}
 
 }
--- backend/src/main/java/com/morjo/model/dto/Quiz.java
+++ backend/src/main/java/com/morjo/model/dto/Quiz.java
@@ -2,20 +2,27 @@ package com.morjo.model.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Quiz {
     private long quizId;
     private String content;
-    private List<String> options = new ArrayList<>();
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String answer;
     private long createdUserId;
     private Timestamp createdAt;
-
-    public void setOptions(String option) {
-        options.add(option);
-    }
+    
 }
