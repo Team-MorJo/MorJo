5e06d405583887b8878546d24a721288e6d25f27
puranium <puranium235@gmail.com>
2024-11-20 14:49:05 +0900

    refactor: session 사용해서 join

--- backend/src/main/java/com/morjo/controller/LoginController.java
+++ backend/src/main/java/com/morjo/controller/LoginController.java
@@ -46,11 +46,22 @@ public class LoginController {
     }
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody User user, @SessionAttribute long kakaoId) {
+    public ResponseEntity<?> join(@RequestBody User user, HttpSession session) {
+        Long kakaoId = (Long) session.getAttribute("kakaoId");
+
+        if (kakaoId == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("가입중인 유저가 아닙니다");
+        }
+
         boolean success = oAuthService.join(user, kakaoId);
 
-        // !TODO 따로 에러를 줘야하는 상황 : 닉네임 중복, accessToken 없음
-        return success ? ResponseEntity.status(HttpStatus.CREATED).build()
-                : ResponseEntity.status(HttpStatus.CONFLICT).build();
+        // !TODO 따로 에러를 줘야하는 상황 : 닉네임 중복
+        if (success) {
+            session.removeAttribute("kakaoId");
+            session.setAttribute("userId", user.getUserId());
+            ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 닉네임입니다");
     }
 }
